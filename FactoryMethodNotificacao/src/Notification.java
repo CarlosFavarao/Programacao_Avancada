285466d13285f5e969214ff1496b675269c8cf4f
Carlos Favarão <130505237+CarlosFavarao@users.noreply.github.com>
2025-03-07 21:12:22 -0300

    Update Notification.java

--- FactoryMethodNotificacao/src/Notification.java
+++ FactoryMethodNotificacao/src/Notification.java
@@ -1,4 +1,4 @@
 public interface Notification {
-    void send(String message);
+    void send(String message);//essa classe padroniza a criação dos métodos nas classes, as mesmas a implementam.
 }
 
